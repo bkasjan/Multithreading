ac6fd5e59f0957514902927255d34a9aba5f62c5
bkasjan <b@b.com>
2016-07-14 09:28:11 +0200

    Add operation cancelation and continuation

--- Multithreading/OperationCancellations/Program.cs
+++ Multithreading/OperationCancellations/Program.cs
@@ -2,16 +2,72 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace OperationCancellations
 {
+    /// <summary>
+    /// Demonstrate: Cancelations and continuations
+    /// </summary>
     class Program
     {
         static void Main(string[] args)
         {
-            //TODO
-            throw new NotImplementedException();
+            // Cancelations using CancellationToken and continuation
+            var cancelSource = new CancellationTokenSource();
+            var cancelToken = cancelSource.Token;
+
+            var task1 = Task.Factory.StartNew(() =>
+            {
+                DoSomeAction(cancelToken);
+            });
+
+            // Continuation task of task1
+            var task2 = task1.ContinueWith(prev =>
+            {
+                Console.WriteLine("Let's continue after 1st task");
+                for (int i = 0; i < 5; i++)
+                {
+                    Console.WriteLine("Doing additional work");
+                    Thread.Sleep(100);
+                }
+            });
+
+            cancelSource.Cancel();
+
+            try
+            {
+                task1.Wait();
+            }
+            catch (AggregateException ex)
+            {
+                if (ex.InnerException is OperationCanceledException)
+                    Console.WriteLine("Task action was cancelled");
+            }
+
+            Console.ReadKey();
+        }
+
+        /// <summary>
+        /// Some actions to do. In the middle checking if task is being cancelled.
+        /// </summary>
+        /// <param name="cancelToken"></param>
+        static void DoSomeAction(CancellationToken cancelToken)
+        {
+            for (int i = 0; i < 5; i++)
+            {
+                Console.WriteLine("Working");
+                Thread.Sleep(1000);
+            }
+
+            cancelToken.ThrowIfCancellationRequested();
+
+            for (int i = 0; i < 5; i++)
+            {
+                Console.WriteLine("Doing rest of the work");
+                Thread.Sleep(1000);
+            }
         }
     }
 }
